7d641c8d4e567a792bc32f13fcdef6c562e4d241
Della <109548222+Della955@users.noreply.github.com>
2023-08-09 16:45:08 -0400

    Update models.py

--- stable_app/models.py
+++ stable_app/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from user_app.models import User 
 # Create your models here.
 class Stable(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="stable_id")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="stable")
     name = models.CharField() 
