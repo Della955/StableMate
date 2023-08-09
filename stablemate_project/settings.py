a4b27801b6af3dfd2c80630a898ff0bbfef3395a
della955 <dellaowens9@gmail.com>
2023-08-09 12:21:30 -0400

    updated settings

--- stablemate_project/settings.py
+++ stablemate_project/settings.py
@@ -11,16 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from dotenv import dotenv_values
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = dotenv_values(".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xqg1x-^(bu4$i&eh(cv3sxvzl_0$d_dapn3^a+0=1n4+49kv&%'
+SECRET_KEY = env.get("DJANGO_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
