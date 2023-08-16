40d20c4f611d031d9c565b8979885d02147676ff
della955 <dellaowens9@gmail.com>
2023-08-16 10:03:55 -0400

    update

--- frontend/stablemate/src/components/HomePage.jsx
+++ frontend/stablemate/src/components/HomePage.jsx
@@ -9,19 +9,19 @@ export const HomePage = () => {
 
   
 
-  const { data, isLoading, errorMessage } = useWeatherBit({
-  key: 'c2365a23637a412695168463c658856c',
-  lat: '48.137154',
-  lon: '11.576124',
-  lang: 'en',
-  unit: 'M', // values are (M,S,I)
-});
+//   const { data, isLoading, errorMessage } = useWeatherBit({
+//   key: 'c2365a23637a412695168463c658856c',
+//   lat: '48.137154',
+//   lon: '11.576124',
+//   lang: 'en',
+//   unit: 'M', // values are (M,S,I)
+// });
 
   return (
     <div>
       <h1>Welcome {user ? user : null}</h1>
       <h3>Here is your 5 day weather outlook</h3>
-      <ReactWeather
+      {/* <ReactWeather
       isLoading={isLoading}
       errorMessage={errorMessage}
       data={data}
@@ -29,7 +29,7 @@ export const HomePage = () => {
       locationLabel="Munich"
       unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
       showForecast
-    />
+    /> */}
 
 
           
